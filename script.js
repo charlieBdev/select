4c326095f8d5d48d0a15e8d3a3f9aa1a4f2e33ec
charliebdev <charliebdev@hotmaill.com>
2026-05-09 20:36:53 +0100

    Fix DM

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ document.querySelectorAll("form").forEach(f => f.onsubmit = e => {
 
 // add a click to demo-heading that will change bs-theme to dark and light
 document.querySelector(".demo-heading").addEventListener("click", () => {
-  const body = document.body;
-  if (body.getAttribute("data-bs-theme") === "dark") {
-    body.setAttribute("data-bs-theme", "light");
+  const themeContainer = document.querySelector("[data-bs-theme]");
+  if (themeContainer.getAttribute("data-bs-theme") === "dark") {
+    themeContainer.setAttribute("data-bs-theme", "light");
   } else {
-    body.setAttribute("data-bs-theme", "dark");
+    themeContainer.setAttribute("data-bs-theme", "dark");
   }
 });
